78625baf687b79527d68d5d909ecddaa0540124a
MichaelMarrero <michael.andre.marrero@gmail.com>
2013-11-19 15:46:56 -0600

    First Commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            var randomizer = new Random();
+            var choice = randomizer.Next(1, 10);
+            if (choice == 1)
+            {
+                Console.WriteLine("Hello " + name);
+            }
+            else
+            {
+                Console.WriteLine("Hello " + name + " How are you?");
+            }
+
+            Console.ReadLine();
+                
+        }
+    }
+}
